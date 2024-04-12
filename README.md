8f7041257b110345e57afcdfcc1168164f8e2c0f
tlaceby <tylerbadgertutorials@gmail.com>
2024-04-11 21:33:21 -0600

    Added impl

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/ZLHpg3lN)
+
 # Quicksort
 
 Implement an iterative (no recursive calls) version of quicksort. Use the
@@ -14,3 +15,11 @@ recursive call considers.
 Analyse the time complexity of your implementation and give a $\Theta$ bound for
 its worst-case runtime. Add your answer, including your reasoning, to this
 markdown file.
+
+## Resources
+
+I heavily used the youtube channel: CS Dojo for understanding of the quicksort algorithm.
+https://www.youtube.com/watch?v=0SkOjNaO1XY
+
+I also found a useful article/post regarding the implimentation which I think helped me with the js implimentation.
+https://www.simplilearn.com/tutorials/data-structure-tutorial/quick-sort-algorithm
--- code.js
+++ code.js
@@ -1,3 +1,44 @@
+/**
+ * @param {number[]} array which will be mutated in place.
+ */
 function quicksort(array) {
-    return array;
+  const stack = []; // 4 keeping track of whatever subarrays need to be sorted.
+  // [startIndex, endIndex] for section which needs to be partitioned. Kinda like a tuple.
+  stack.push([0, array.length - 1]);
+
+  // While we still need to partition the array we will have values in stack.
+  while (stack.length > 0) {
+    const [start, end] = stack.pop();
+
+    if (start > end) continue; // already sorted propery.
+
+    const pivot = partition(array, start, end);
+
+    stack.push([start, pivot - 1]);
+    stack.push([pivot, end]);
+  }
+
+  return array;
+}
+
+function partition(array, start, end) {
+  const pivot = array[end];
+  let i = start - 1;
+
+  // Foreach subarray, swap out of place elements from the pivot to the end of the section
+  for (let j = start; j < end; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      swap(array, i, j);
+    }
+  }
+
+  swap(array, i + 1, end);
+  return i;
+}
+
+function swap(arr, a, b) {
+  const tmp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = tmp;
 }
